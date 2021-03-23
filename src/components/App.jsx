906fd5c6d8b7f690c865a7285e2f6bcc215c63a3
Sayanti Sarkar <68384931+sayantisarkar@users.noreply.github.com>
2021-03-23 11:32:32 +0400

    Using contacts.js file To use data import, instead of hardcoding values and added Card component seperately

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,29 @@
 import React from "react";
+import contacts from "../contacts.js";
+import Card from "./Card.jsx";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card
+        name={contacts[0].name}
+        imgURL={contacts[0].imgURL}
+        phone={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        imgURL={contacts[1].imgURL}
+        phone={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        imgURL={contacts[2].imgURL}
+        phone={contacts[2].phone}
+        email={contacts[2].email}
+      />
     </div>
   );
 }
